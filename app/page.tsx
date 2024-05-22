6648b0742790c68871f509b1c0acc0f2bfc76c6a
HarmonyMind <jill.janqueiro@gmail.com>
2024-05-22 18:30:19 +0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,196 @@
-import Image from "next/image";
+'use client'
+
+import React, { useEffect, useState } from 'react';
+
+import {
+  ConnectButton,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+
+import { useChainId, useAccount, useBalance, useWriteContract, useWaitForTransactionReceipt } from "wagmi";
+import { Address, parseUnits, formatUnits } from 'viem'
 
 export default function Home() {
+  // initial state:
+  const [currentUSDTbalance, setcurrentUSDTbalance] = useState(0);
+  const [selectedAmount, setselectedAmount] = useState("0.5");
+  const [paymentTrxnId, setpaymentTrxnId] = useState("");
+  const [paymentReceipt, setpaymentReceipt] = useState<{ status: string, transaction_id: Address }>({ status: '', transaction_id: '' as Address });
+  const chainId = useChainId();
+  const { isConnected, address } = useAccount();
+  const useBalanceResult = useBalance({
+    address: address as Address,
+    token: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F' as Address,  /// USDT contract address
+  })
+
+  const { writeContractAsync, status, isSuccess, isError, error: transfer_error } = useWriteContract();
+
+  const TransactionReceipt = useWaitForTransactionReceipt({
+    hash: paymentTrxnId as Address,
+  })
+
+  useEffect(() => {
+    if (useBalanceResult) {
+      console.log(useBalanceResult.data);
+      if (useBalanceResult.data?.value)
+        setcurrentUSDTbalance(Number(formatUnits(useBalanceResult.data?.value!, 6)));
+    }
+  }, [useBalanceResult]);
+
+
+  // This useEffect triggers only when TransactionReceipt changes value
+  useEffect(() => {
+    console.log('TransactionReceipt is');
+
+    if (TransactionReceipt) {
+      if (TransactionReceipt.data && (TransactionReceipt.status === 'success')) {
+        if (TransactionReceipt.data.transactionHash === (paymentTrxnId as Address)) {
+          console.log('Transaction ' + paymentTrxnId + ' was successfull');
+
+          let _receipt = {
+            status: 'success',
+            transaction_id: TransactionReceipt.data.transactionHash
+          }
+          setpaymentReceipt(_receipt);
+          // clear triggering transaction_id. 
+          setpaymentTrxnId('');
+        }
+      }
+    }
+  }, [TransactionReceipt])
+
+  // clear previous results on each onConnect event
+  useEffect(() => {
+    setpaymentTrxnId('');
+    setpaymentReceipt({ status: '', transaction_id: '' as Address });
+  }, [isConnected])
+
+
+  // async format
+  const FunctionSend = async () => {
+
+    console.log('FunctionSend invoked');
+
+    // clear previous results
+    setpaymentTrxnId('');
+    setpaymentReceipt({ status: '', transaction_id: '' as Address });
+
+    const abi = [
+      {
+        type: 'function',
+        name: 'approve',
+        stateMutability: 'nonpayable',
+        inputs: [
+          { name: 'spender', type: 'address' },
+          { name: 'amount', type: 'uint256' },
+        ],
+        outputs: [{ type: 'bool' }],
+      },
+      {
+        type: 'function',
+        name: 'transferFrom',
+        stateMutability: 'nonpayable',
+        inputs: [
+          { name: '_from', type: 'address' },
+          { name: '_to', type: 'address' },
+          { name: 'amount', type: 'uint256' },
+        ],
+        outputs: [{ type: 'bool' }],
+      },
+
+    ]
+
+    try {
+      let trxId = await writeContractAsync({
+        abi,
+        address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',  /// USDT contract address 
+        functionName: 'approve',
+        args: [
+          address,
+          parseUnits(selectedAmount, 6),],
+        chainId: chainId,
+      });
+
+      console.log(trxId as string);
+
+      trxId = await writeContractAsync({
+        abi,
+        address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',  /// USDT contract address 
+        functionName: 'transferFrom',
+        args: [
+          address as Address, // => address FROM,
+          process.env.RECEPIENT_ADDRESS, // => address TO,
+          parseUnits(selectedAmount, 6),],
+        chainId: chainId,
+      });
+
+      console.log(trxId as string);
+
+      // need to get confirmation that transaction was successful
+      // so we set transaction id for which useWaitForTransactionReceipt will be triggered.
+      setpaymentTrxnId(trxId as string);
+    } catch (e) {
+      console.log(e)
+    }
+
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <RainbowKitProvider>
+        <h1 className='text-center text-3xl font-extrabold'>NextJS Web3 application</h1><br />
+        <div className='flex flex-row justify-center items-center'>
+          <ConnectButton />
         </div>
-      </div>
+      </RainbowKitProvider>
+      <section className='mt-6'>
+        <div className='flex flex-row justify-evenly text-center mb-4'>
+          Your current balance of USDT = {currentUSDTbalance} USDT
+        </div>
+        <h2 className='text-center text-2xl font-bold mb-4'>Your crypto transfer:</h2>
+        <div className='flex flex-row justify-evenly mb-4'>
+          <select name="selectedAmount"
+            value={selectedAmount} // ...force the select's value to match the state variable...
+            onChange={e => setselectedAmount(e.target.value)} // ... and update the state variable on any change!
+          >
+            <option value="0.1">0.1</option>
+            <option value="0.5">0.5</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+          </select>
+          <span>
+            <b>USDT</b>
+          </span>
+        </div>
+        <div>
+          Recipient wallet address = {process.env.RECEPIENT_ADDRESS}
+        </div>
+        <div>
+          Your wallet address = {address as string}
+        </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {isConnected && (
+          <>
+            <button className='hover:bg-green-600 bg-green-400 rounded-md p-2 w-full font-semibold'
+              onClick={FunctionSend}>Go!</button>
+            <div>
+              Current transaction status is {status}
+            </div>
+            <div>
+              {isSuccess && "Waiting for reciept..."}
+            </div>
+            <div>
+              {isError && ("We got error:" + transfer_error.message)}
+            </div>
+            <div>
+              {(paymentReceipt.status === 'success') && (
+                <>
+                  Reciept: transaction hash is {paymentReceipt.transaction_id}
+                </>)}
+            </div>
+          </>)}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        RECEPIENT_ADDRESS: process.env.RECEPIENT_ADDRESS, // pulls from .env file
+    },
+    webpack: config => {
+        config.externals.push('pino-pretty', 'lokijs', 'encoding');
+        return config;
+    },
+};
 
 export default nextConfig;
